beabf726f073496a2da7e2b614891b7dd0b8a11d
MaarLop <mariel.lopez@d3sistemas.com.ar>
2021-04-20 22:39:05 -0300

    Se realizó cambio en service para traer paginados los datos

--- app/src/app/services/review.service.ts
+++ app/src/app/services/review.service.ts
@@ -1,18 +1,25 @@
 import { Injectable } from '@angular/core';
 import{ HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Review } from '../core/models/review-model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReviewService {
-  protected basePath = 'http://localhost:8090/review';
+  protected basePath = 'http://localhost:8090/review?sort=id&order=desc';
 
   constructor(private httpClient: HttpClient) { }
 
-  public getReviews() : Observable<any> {
-    const path = this.basePath;
+  public getReviews(size?:number, page?: number) : Observable<any> {
+    const pageAndSize = size === null && page === null ? '' : `&page=${page}&size=${size}`;
+    const path = `${this.basePath}${pageAndSize}`;
     return this.httpClient.get(path);
+  }
+
+  public save(body: Review){
+    const path = this.basePath + '/save';
+    return this.httpClient.post(path, body);
   }
   
 }
