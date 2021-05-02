6051d8addefba7fe5479ede451eacb070f7ed818
MaarLop <mariel.lopez@d3sistemas.com.ar>
2021-05-02 16:19:55 -0300

    Refactor

---
+++ app/src/app/core/review-list/review-list.component.ts
@@ -0,0 +1,57 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { BehaviorSubject } from 'rxjs';
+import { ReviewService } from 'src/app/services/review.service';
+import { Review } from '../models/review-model';
+
+@Component({
+    selector: 'app-review-list',
+    templateUrl: './review-list.component.html',
+    styleUrls: ['./review-list.component.scss']
+})
+export class ReviewListComponent implements OnInit {
+
+    loading = true;
+
+    reviews$ = new BehaviorSubject<Review[]>([]);
+    size: number = 5;
+    page: number = 0;
+    finished = false;
+    showSpinner = false;
+
+    @Input() filter$?: BehaviorSubject<string>;
+
+    constructor(private reviewService: ReviewService, public snackBar: MatSnackBar){ }
+
+    ngOnInit(): void {
+        if(this.filter$ !== undefined){
+            this.filter$.subscribe((_)=>{
+                this.getReviews();
+            });
+        }
+        else{
+            this.getReviews();
+        }
+
+    }
+
+    getReviews(){
+        if(this.finished) return;
+
+        this.reviewService.getReviews(this.size, this.page, this.filter$?.value).subscribe((response)=>{
+            const reviewList = this.reviews$.value;
+            this.reviews$.next([...reviewList, ...response.content]);
+            this.finished = response.last;
+            this.showSpinner = !this.finished;
+            this.page+=1;
+        });
+    }
+
+    onScroll(){
+        setTimeout(() => {
+            this.getReviews();
+            this.showSpinner = false;
+        }, 2000);
+    }
+
+}
--- app/src/app/home-page/home-page.component.ts
+++ app/src/app/home-page/home-page.component.ts
@@ -3,7 +3,6 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 import { BehaviorSubject } from 'rxjs';
 import { Review } from '../core/models/review-model';
 import { ReviewService } from '../services/review.service';
-// import * as _ from 'loadash' es una libreria
 
 @Component({
     selector: 'app-home-page',
@@ -13,9 +12,6 @@ import { ReviewService } from '../services/review.service';
 export class HomePageComponent {
 
     loading = true;
-    hashtags1: string []= ['#Accion', '#Drama', '#Netflix', '#Hoy']
-    hashtags2: string []= ['#hashtags']
-    hashtags3: string []= ['#SorryButNoTSorry', '#estoEsUnaReseña', '#malisimaaa']
 
     reviews$ = new BehaviorSubject<Review[]>([]);
 
@@ -24,30 +20,7 @@ export class HomePageComponent {
     finished = false;
     showSpinner = false;
 
-    constructor(private reviewService: ReviewService, public snackBar: MatSnackBar){ }
-
-    ngOnInit(): void {
-        this.getReviews();
-    }
-
-    getReviews(){
-        if(this.finished) return;
-
-        this.reviewService.getReviews(this.size, this.page).subscribe((response)=>{
-            const reviewList = this.reviews$.value;
-            this.reviews$.next([...reviewList, ...response.content]);
-            this.finished = response.last;
-            this.showSpinner = !this.finished;
-            this.page+=1;
-        });
-    }
-
-    onScroll(){
-        setTimeout(() => {
-            this.getReviews();
-            this.showSpinner = false;
-        }, 2000);
-    }
+    constructor(public snackBar: MatSnackBar){ }
 
     newReview(review: Review){
         console.log(review)
