b945632e33e8767ef4f5afe0be3f04c9d64758a3
mcastello <mcastello@nacionservicios.com.ar>
2021-05-23 13:00:41 -0300

    Add tests

--- app/src/app/core/comment-list/comment-list.component.spec.ts
+++ app/src/app/core/comment-list/comment-list.component.spec.ts
@@ -1,6 +1,11 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { AuthModule } from '@auth0/auth0-angular';
 
 import { CommentListComponent } from './comment-list.component';
+import { FormBuilder } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Overlay } from '@angular/cdk/overlay';
 
 describe('CommentListComponent', () => {
   let component: CommentListComponent;
@@ -8,7 +13,19 @@ describe('CommentListComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CommentListComponent ]
+      declarations: [ CommentListComponent ],
+      imports: [
+        HttpClientTestingModule,
+        AuthModule.forRoot({
+          domain: 'dev-d8bhv2ic.us.auth0.com',
+          clientId: 'OPF2R4rQ8M3OHOgZDooC4BJoK5kr12l2'
+        }),
+      ],
+      providers: [
+        FormBuilder,
+        MatSnackBar,
+        Overlay
+      ]
     })
     .compileComponents();
   });
--- app/src/app/core/comment/comment.component.spec.ts
+++ app/src/app/core/comment/comment.component.spec.ts
@@ -1,14 +1,35 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { AuthModule } from '@auth0/auth0-angular';
 
 import { CommentComponent } from './comment.component';
 
 describe('CommentComponent', () => {
   let component: CommentComponent;
   let fixture: ComponentFixture<CommentComponent>;
+  let mockComment: any = 
+    {
+      message: "Muy buena",
+      user: {
+        id: 1,
+        name: "Martín",
+        lastName: "Castello",
+        email: "castellomn94@gmail.com",
+        userName: "castellomn94",
+        avatar: "https://lh3.googleusercontent.com/a/AATXAJxDiyks0qyYHf6oWUsFWkUWk5JqpU_zBdksddLj=s96-c"
+      }
+    }
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CommentComponent ]
+      declarations: [ CommentComponent ],
+      imports: [
+        HttpClientTestingModule,
+        AuthModule.forRoot({
+          domain: 'dev-d8bhv2ic.us.auth0.com',
+          clientId: 'OPF2R4rQ8M3OHOgZDooC4BJoK5kr12l2'
+        }),
+      ],
     })
     .compileComponents();
   });
@@ -16,6 +37,7 @@ describe('CommentComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(CommentComponent);
     component = fixture.componentInstance;
+    component.comment = mockComment;
     fixture.detectChanges();
   });
 
