09f72ce5ad20e5a9c637dd79c890100c46d3008a
MaarLop <mariel.lopez@d3sistemas.com.ar>
2021-06-07 19:43:24 -0300

    Fixes chat

--- app/src/app/chat/chat.component.ts
+++ app/src/app/chat/chat.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
+import { Pageable } from '../core/models/pageable.model';
 import { User } from '../core/models/user.model';
 import { UserService } from '../services/user.service';
 
@@ -18,14 +19,14 @@ export class ChatComponent implements OnInit{
   constructor(protected userService: UserService) { }
 
     ngOnInit(): void {
-        this.userService.getFollowings(1).subscribe((followings: any[])=>{
-            const followingsUser = followings.map((f)=> f.to)
+        this.userService.getFollowings(sessionStorage.getItem('userName')).subscribe((followings: Pageable)=>{
+            const followingsUser = followings.content.map((f)=> f.to)
             this.userList.push(...followingsUser)
         });
     }
 
   sendMessage(): void {
-    const sender = sessionStorage.getItem('user')
+    const sender = sessionStorage.getItem('userName')
     this.messageList.push({message: this.message, userName: sender, mine: true});
     this.messageList.push({message: 'como estas', userName: 'user', mine: false});
     this.message = '';
