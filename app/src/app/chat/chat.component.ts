2ffdcc28a6ac661a6f76558106e902fb8a924603
MaarLop <mariel.lopez@d3sistemas.com.ar>
2021-06-08 20:24:28 -0300

    Chat

--- app/src/app/chat/chat.component.ts
+++ app/src/app/chat/chat.component.ts
@@ -1,35 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
+import { BehaviorSubject } from 'rxjs';
 import { Pageable } from '../core/models/pageable.model';
 import { User } from '../core/models/user.model';
 import { UserService } from '../services/user.service';
+import { WebSocketService } from '../services/web-socket.service';
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.scss']
 })
-export class ChatComponent implements OnInit{
+export class ChatComponent implements OnInit, OnDestroy{
   message = '';
   messageList: {message: string, userName: string, mine: boolean}[] = [];
   userList: any[] = [];
+  sender = sessionStorage.getItem('userName');
+  receiber = '';
+  chatTitle = '';
 //   socket: any;
 
   send = faPaperPlane
-  constructor(protected userService: UserService) { }
+  constructor(protected userService: UserService, public webSocketService: WebSocketService) { }
 
-    ngOnInit(): void {
-        this.userService.getFollowings(sessionStorage.getItem('userName')).subscribe((followings: Pageable)=>{
-            const followingsUser = followings.content.map((f)=> f.to)
-            this.userList.push(...followingsUser)
-        });
-    }
+  ngOnInit(): void {
+    this.userService.getFollowings(sessionStorage.getItem('userName')).subscribe((followings: Pageable)=>{
+        const followingsUser = followings.content.map((f)=> f.to)
+        this.userList.push(...followingsUser)
+    });
+    this.webSocketService.openWebSocket();
+  }
 
   sendMessage(): void {
-    const sender = sessionStorage.getItem('userName')
-    this.messageList.push({message: this.message, userName: sender, mine: true});
-    this.messageList.push({message: 'como estas', userName: 'user', mine: false});
-    this.message = '';
+    if(this.receiber){
+      const msg = {message : this.message, userName: this.sender, to: this.receiber, mine: true}
+      this.webSocketService.sendMessage(msg);
+      // this.messageList.push(msg);
+      // this.messageList.push({message: 'como estas', userName: this.receiber, mine: false});
+      this.message = '';
+    }
+    else{
+      alert('seleccione un destinatario');
+    }
+  }
+
+  sendMessageTo(user){
+    this.receiber= user.userName;
+    this.chatTitle = `${user.name} ${user.lastName}`
+    this.messageList = [];// get mensajes enviados
+  }
+
+  ngOnDestroy(): void {
+    this.webSocketService.closeWebSocket();
   }
 
 }
